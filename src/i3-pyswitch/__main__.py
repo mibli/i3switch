992287cacc31f91f214bfe804d7ef33e78cacefe
Milosz Blizniak <mibli@gmx.com>
2016-10-09 10:39:13 +0200

    feat(py): initial command line version
    - feat: cli interface
    - feat: switch to tab number
    - feat: switch to next tab (focus next)

---
+++ src/i3-pyswitch/__main__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from controller import Controller
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Tabs-concious switching')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-d', '--direction', type=str,
+                       choices=controller.directions,
+                       help='Switch to [direction] of focused window (ignores tabs)')
+    parser.add_argument('-t', '--tab', action='store_true',
+                        help='For use with -d, to switch tabs')
+    parser.add_argument('-p', '--parent', action='store_true',
+                        help='For use with -d, to switch tabs')
+    group.add_argument('-n', '--number', type=int,
+                       help='Try to switch to n-th tab of focused container')
+    args = parser.parse_args()
+
+    def print_help_and_die(message):
+        print(message)
+        parser.print_help()
+        exit(1)
+
+    if args.direction is None:
+        if args.tab:
+            print_help_and_die("Use --tab only with -d or --direction")
+        if args.parent:
+            print_help_and_die("Use --parent only with -d or --direction")
+    if args.number is not None and args.number <= 0:
+        print_help_and_die("Tab indexes start from 1")
+
+    i3 = Controller()
+
+    if args.number:
+        i3.switch_to_tab(args.number)
+    elif args.direction is not None:
+        if args.tabs:
+            i3.switch_tabs(args.direction)
+        else:
+            i3.switch(args.direction, args.parent)
