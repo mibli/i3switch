86c134eaca99b03dc7c080df706f4ce89f95a342
Milosz Blizniak <mibli@gmx.com>
2018-07-05 18:27:42 +0200

    refactor(cpp): move call to its own module

---
+++ src/utils/call.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+
+std::string call(std::string const &command, bool strip_last = true)
+{
+    FILE *pipe = popen(command.c_str(), "r");
+    if (pipe == nullptr)
+    {
+        printf("failed to create pipe for '%s'", command.c_str());
+        return {};
+    }
+
+    std::stringstream stream;
+    char buffer[256];
+
+    while(fgets(buffer, sizeof(buffer), pipe) != nullptr)
+        stream << buffer;
+
+    auto rc = pclose(pipe);
+    if (rc > 0)
+        printf("'%s' exited with an error: %d", command.c_str(), rc);
+
+    std::string result = stream.str();
+    if (strip_last)
+    {
+        result.erase(result.size() - 1);
+    }
+
+    return result;
+}
