705a9cda4cd7459b7d0eaf47ae44d980554039a9
Milosz B <milosz.b@gmx.com>
2018-07-05 18:27:42 +0200

    call: moved to utils/call.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "utils/logging.hpp"
 #include "utils/stringf.hpp"
+#include "utils/call.hpp"
 #include "connection/i3client.hpp"
 #include "i3tree.hpp"
 
@@ -17,33 +18,6 @@
 #include <thread>
 #include <chrono>
 
-std::string call(std::string const &command, bool strip_last = true)
-{
-    FILE *pipe = popen(command.c_str(), "r");
-    if (pipe == nullptr)
-    {
-        printf("failed to create pipe for '%s'", command.c_str());
-        return {};
-    }
-
-    std::stringstream stream;
-    char buffer[256];
-
-    while(fgets(buffer, sizeof(buffer), pipe) != nullptr)
-        stream << buffer;
-
-    auto rc = pclose(pipe);
-    if (rc > 0)
-        printf("'%s' exited with an error: %d", command.c_str(), rc);
-
-    std::string result = stream.str();
-    if (strip_last)
-    {
-        result.erase(result.size() - 1);
-    }
-
-    return result;
-}
 
 int main(int argc, char *argv [])
 {
