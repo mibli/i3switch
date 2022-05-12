fa619eaafcedb0b15f82031dcadc47c0df923561
Miłosz <milosz.blizniak@bcf-software.com>
2022-05-13 01:12:03 +0200

    First iteration of working floating window switching
    * Direction based switching is still broken

--- src/linear.hpp
+++ src/linear.hpp
@@ -8,7 +8,7 @@ enum class Direction { PREV, NEXT, INVALID = 0xFF };
 
 class Sequence {
 public:
-    Sequence(std::vector<std::string> const &ids, size_t current);
+    Sequence(std::vector<std::string> const &ids = {}, size_t current = 0);
 
     std::string const *next(Direction direction) const;
     std::string const *first(Direction direction) const;
