cff377b96b2cbed6e0ed071379e5f8eb09134a96
Milosz Blizniak <mibli@gmx.com>
2025-06-15 12:39:08 +0200

    build(py): build binary with pyinstaller
    Binary is now converted to a single executable to follow distribution
    pattern of other languages.

---
+++ python/build.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+build=
+install=false
+
+for i in "$@"; do
+    case "$i" in
+        rebuild)
+            rm -rf build i3switch.spec
+            shift
+            ;;
+        debug)
+            build="debug"
+            shift
+            ;;
+        release)
+            build="release"
+            shift
+            ;;
+        install)
+            install=true
+            exit 0
+            ;;
+        -h|--help|*)
+            echo "Usage: $0 (debug|release) [rebuild] [install]"
+            exit 0
+            ;;
+    esac
+done
+
+: "${version:="0.0.0"}"
+if [ "$version" = "0.0.0" ]; then
+    git_version=$(git describe --tags --abbrev=0 --match="python-v[0-9]*" 2>/dev/null)
+    if [ -z "$git_version" ]; then
+        echo "[WARN] No version tag found, using 0.0.0"
+    else
+        version=${git_version#rust-v}
+        sed -i "s|0\.0\.0|$version|g" i3switch/__main__.py
+    fi
+fi
+
+for dep in pyinstaller $(cat requirements.txt); do
+    if pip freeze | grep -q "^$dep"; then
+        echo "$dep is installed."
+    else
+        echo "[ERROR] Dependency $dep is not missing." >&2
+        echo "Please install it using pip or package manager."
+        exit 1
+    fi
+done
+
+if [ -n "$build" ]; then
+    echo "Building i3switch in $build mode..."
+    pyinstaller --onefile --name i3switch --distpath ./build/bin run.py || {
+        echo "[ERROR] Build failed." >&2
+        exit 1
+    }
+fi
+
+if [ "$install" = true ] && [ -f "build/bin/i3switch" ]; then
+    echo "Installing i3switch..."
+    sudo cp dist/i3switch /usr/local/bin/ || {
+        echo "[ERROR] Installation failed." >&2
+        exit 1
+    }
+    exit 0
+fi
+
+echo "All tasks completed successfully."
