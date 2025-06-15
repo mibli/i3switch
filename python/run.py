fa4ada819e4c374aa044759c127b31467e0db69c
Miłosz <milosz.blizniak@bcf-software.com>
2025-06-15 12:39:08 +0200

    python: Added build to a binary with PyInstaller

---
+++ python/run.py
@@ -0,0 +1,6 @@
+# This file is only for pyinstaller to run the application and preserve the module structure.
+
+from i3switch.__main__ import entrypoint
+
+if __name__ == "__main__":
+    entrypoint()
