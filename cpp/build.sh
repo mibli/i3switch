ecb4b0a9e85fd93bb609cc6056077d0f37133326
Miłosz <milosz.blizniak@bcf-software.com>
2025-06-15 13:45:43 +0200

    cpp: Cleaned up and updated build

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,27 +1,55 @@
-#!/bin/sh
-while [ -n "$1" ]; do
-    case "$1" in
-        "rebuild")
+#!/bin/bash
+build_type=
+install=
+
+for i in "$@"; do
+    case "$i" in
+        rebuild)
             rm -rf build
             mkdir -p build
             ;;
-        "debug")
-            build_type="Debug"
+        debug)
+            build_type=Debug
             ;;
-        "release")
-            build_type="Release"
+        release)
+            build_type=Release
             ;;
-        ?)
-            echo "Usage: ./build.sh [(debug|release)] [rebuild]"
+        install)
+            build_type=Release
+            install=install
+            ;;
+        -h|--help|*)
+            echo "Usage: ./build.sh (debug|release) [rebuild] [install]"
+            exit 0
     esac
     shift
 done
 
-: ${build_type="Release"}
-: ${install:=false}
+if [ -z "$build_type" ] ; then
+    echo "Usage: ./build.sh (debug|release) [rebuild] [install]"
+    exit 1
+fi
 
-mkdir -p build
-pushd build
-cmake .. -DCMAKE_BUILD_TYPE="$build_type"
-make
-popd
+: "${version:="0.0.0"}"
+if [ "$version" = "0.0.0" ]; then
+    git_version=$(git describe --tags --abbrev=0 --match="cpp-v[0-9]*" 2>/dev/null)
+    if [ -z "$git_version" ]; then
+        echo "[WARN] No version tag found, using 0.0.0"
+    else
+        version=${git_version#cpp-v}
+    fi
+fi
+
+if [ -n "$build_type" ]; then
+    echo "Building in $build_type mode"
+    set -e
+    mkdir -p build
+    cmake -B build -DGIT_REPO_VERSION="$version" -DCMAKE_BUILD_TYPE="$build_type"
+    make -C build
+    if [ -n "$install" ]; then
+        make -C build install
+    fi
+    set +e
+fi
+
+echo "Build completed successfully"
